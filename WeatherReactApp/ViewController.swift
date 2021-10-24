b71a7c9f9bad5ca0981503b7e41841dca21a2c2f
Darinka <dpolevyk@post.cz>
2021-10-24 14:05:15 +0200

    Initial Commit

---
+++ WeatherReactApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherReactApp
+//
+//  Created by Darinka on 24.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
