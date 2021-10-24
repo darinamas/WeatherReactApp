f5e3eb00c15d8727c8e17d6793d15e8a1ad38145
Darinka <dpolevyk@post.cz>
2021-10-24 14:23:11 +0200

    Added view, label and button

--- WeatherReactApp/VC/ViewController.swift
+++ WeatherReactApp/VC/ViewController.swift
@@ -11,9 +11,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func getWether(_ sender: Any) {
+        print("Weather")
+    }
 }
 
